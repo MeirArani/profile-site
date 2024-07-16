c417909f14086a5c9fc9eb019d04158cd5b01b4a
Zachary Arani <cloud12817@gmail.com>
2024-07-16 16:13:42 +0900

    Working on resume.

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,69 @@
 import Image from "next/image";
+import profileImg from '../public/me_square.webp'
 
 export default function Page() {
   return (
-    <section className="flex flex-col content-center">
-      <h1 className="font-medium text-2xl mb-4 tracking-tighter">Hi. I'm Meir</h1>
+    <section className="flex flex-col items-center">
+      <h1 className="font-medium text-4xl mb-4 tracking-tighter text-center w-fit">Hi.</h1>
       <div className="relative aspect-[3/2] mb-4 lg:-w-2/4">
         <Image  
-      src="/me_square.webp" 
-      fill
-      alt="Test"
+      src={profileImg} 
+      alt="Hi."
       style={{
-        objectFit: 'cover', // cover, contain, none
+        //objectFit: 'cover', // cover, contain, none
+        width: '100%',
+        height: 'auto'
       }}
-      className="rounded-md"
+      priority={true}
+      placeholder="blur"
+      className="rounded-lg"
       ></Image>
       </div>
-      
-      <p className="prose prose-neutral dark:prose-invert">
-        I'm a graduate student at Kyushu University, artist, and aspiring game designer.
-        I develop games, compose music, write stories, and shoot film. I've written screenplays and once taught a AI to write a script (before it was hip!).
+      <h1 className="font-medium text-3xl mb-4 tracking-tighter text-center w-fit">I'm Meir. &nbsp; Nice to meetcha.</h1>
+
+      <p className="prose prose-2xlg">
+        I'm an artist, aspiring game designer, and graduate student at Kyushu University.
+        I also&nbsp;
+        <a href="/games">develop games</a>,&nbsp;
+        <a>shoot pictures</a>,&nbsp;
+        <a>write fiction</a>, and&nbsp;
+        <a>compose music</a> in my spare time. 
+        I've <a>created games for companies</a> and <a href="https://youtu.be/tx22WrInPYY?t=6602">taught an AI to write a movie</a> (before it was cool)!
       </p>
+      <hr className="my-4 border-neutral-50"></hr>
+      <p className="prose prose-neutral">
+        I'm currently researching games accessibility tech for the visually impaired&mdash;designing tools to streamline accessibility feature development. In my free time I design my own game concepts, and ocassionally find the time to develop prototypes. <a>Check out my work for more details.</a>
+      </p>
+      <ul className="font-sm w-full mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <li>
+          <a 
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.instagram.com/mappingthestatic/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">follow me</p>
+          </a>
+        </li>
+      </ul>
     </section>
+  );
+}
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
   );
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,11 +1,43 @@
+import Image from "next/image";
+import profileImg from '../../public/me_work.webp'
+
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
-      <div className="prose prose-neutral">
-        <p className="">
-        This is where I introduce my stuff.
-        </p>
+      <div className="flex flex-row w-full h-48">
+        <div className="flex flex-col w-full pl-2 mt-1">
+          <h1 className="font-medium text-4xl tracking-tighter mb-1 ml-[-1.9px]">Meir Arani</h1>
+          <div className="text-sm">
+            <p className="">Fukuoka, Japan</p>
+            <p>Graduate Student</p>
+            <p className="">Kyushu University</p>
+          </div>
+          <ul className="list-disc list-inside text-xs mt-1 mb-0.5">
+            <li>Game Design</li>
+            <li>Art</li>
+            <li>Programming</li>
+          </ul>
+          <p className="prose">
+              <a href="mailto:myrrh@laughingstock.me">myrrh@laughingstock.me</a>
+            </p>
+        </div>
+        <div className="flex flex-row items-center -ml-4.5">
+          <div className="flex flex-col items-center">
+            <Image
+            src={profileImg} 
+            alt="Why are you hovering over this?"
+            style={{
+              //objectFit: 'cover', // cover, contain, none
+              width: '100%',
+              height: 'auto'
+            }}
+            className="rounded-full"
+            ></Image>
+          </div>
+        </div>
+        
+      </div>
+      
         <hr className="my-6 border-neutral-100"></hr>
         <h2 className="font-medium text-xl mb-1 tracking-tighter">First Thing</h2>
         <p className="text-neutral-600 text-sm">
@@ -13,9 +45,7 @@ export default function Page() {
         </p>
         <p>
           First actual description
-        </p>
-      </div>
-      
+        </p>      
     </section>
   );
 }
