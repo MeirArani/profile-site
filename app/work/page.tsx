e18528b2727d67915ba9d888f3b5cccfa91f9a7f
Zachary Arani <cloud12817@gmail.com>
2024-07-16 23:10:34 +0900

    Resume updates

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -7,14 +7,14 @@ export default function Page() {
       <div className="flex flex-row w-full h-48">
         <div className="flex flex-col w-full pl-2 mt-1">
           <h1 className="font-medium text-4xl tracking-tighter mb-1 ml-[-1.9px]">Meir Arani</h1>
-          <div className="text-sm">
+          <div className="text-sm tracking-wide">
             <p className="">Fukuoka, Japan</p>
             <p>Graduate Student</p>
             <p className="">Kyushu University</p>
           </div>
           <ul className="list-disc list-inside text-xs mt-1 mb-0.5">
             <li>Game Design</li>
-            <li>Art</li>
+            <li>Writing</li>
             <li>Programming</li>
           </ul>
           <p className="prose">
@@ -38,13 +38,15 @@ export default function Page() {
         
       </div>
       
-        <hr className="my-6 border-neutral-100"></hr>
-        <h2 className="font-medium text-xl mb-1 tracking-tighter">First Thing</h2>
+        <hr className="my-4 border-neutral-100"></hr>
+        <h1 className="font-medium text-3xl mb-1 tracking-tighter">Education</h1>
+        <h2 className="font-medium text-xl tracking-tighter">Kyushu University</h2>
+        <h3 className="font-medium text-lg tracking-tighter">Graduate School of Design</h3>
         <p className="text-neutral-600 text-sm">
-          Byline
+          2022-
         </p>
         <p>
-          First actual description
+          Currently researching serious games under <a href="https://hyoka.ofc.kyushu-u.ac.jp/search/details/K002398/english.html">Hiroyuki Matsuguma.</a>
         </p>      
     </section>
   );
