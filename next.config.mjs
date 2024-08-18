95624d472e591576559a4e4bad2d0a55243a48e5
Zachary Arani <cloud12817@gmail.com>
2024-08-18 13:44:45 +0900

    ALL OF THE POLYFILLS!!!

--- next.config.mjs
+++ next.config.mjs
@@ -13,10 +13,29 @@ const nextConfig = {
   },
   webpack: (config, options) => {
     config.resolve.fallback = {
-      https: false,
+      assert: false,
+      buffer: false,
+      console: false,
+      constants: false,
       crypto: false,
+      domain: false,
+      events: false,
       http: false,
-      stream: false
+      https: false,
+      os: false,
+      path: false,
+      punycode: false,
+      process: false,
+      querystring: false,
+      stream: false,
+      string_decoder: false,
+      sys: false,
+      timers: false,
+      tty: false,
+      url: false,
+      util: false,
+      vm: false,
+      zlib: false,
     };
     return config;
   },
