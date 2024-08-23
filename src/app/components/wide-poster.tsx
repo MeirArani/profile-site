8cd37416acbb57986062b00d8f951950fbc4a7ec
Zachary Arani <cloud12817@gmail.com>
2024-08-23 12:05:20 +0900

    Video optimization. Music playback. Logo Design

--- src/app/components/wide-poster.tsx
+++ src/app/components/wide-poster.tsx
@@ -1,11 +1,22 @@
-import Image from "next/image"
+import Image, { StaticImageData } from "next/image"
+import Link from "next/link"
 
 export function FullBanner(props) { 
   return (
-    <div className="relative mt-1 mb-2 h-52 bg-slate-300">
-      <Image 
-          {...props}
-      ></Image>                                     
+    <div>
+      <Link href={props.href} className="block relative w-screen">
+        <div className="relative mt-1 mb-2 h-52 bg-slate-300">
+            <Image 
+                src={props.backgroundImg as StaticImageData} 
+                alt="Tenjin Dash Background"
+                fill
+                className="w-full h-full object-cover blur-[2px]"
+            ></Image>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+            {props.logo}                                    
+            </div>
+          </div>
+        </Link>                                   
     </div>
   )
 }
